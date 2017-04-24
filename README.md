d49e289742128a2cd6506e8d87fbf8f416bec779
Bergwinkl Thomas <bergwinkl@bulwiengesa.de>
2017-04-24 10:26:49 +0200

    added htmlPrefix(Css|Js) option, use standard code style, updated deps

---
+++ README.md
@@ -0,0 +1,4 @@
+# grunt-bower-build
+
+Copies files defined in `bower.json` and creates HTML imports for CSS and JavaScript files.
+
--- tasks/bower-build.js
+++ tasks/bower-build.js
@@ -1,192 +1,185 @@
-'use strict';
-
-var
-  _ = require('underscore'),
-  fs = require('fs'),
-  path = require('path');
-
+var _ = require('lodash')
+var fs = require('fs')
+var path = require('path')
 
 module.exports = function (grunt) {
-  //TODO: add option for uglifyjs
-  //TODO: add option for clean-css
-
   var defaultOptions = {
+    concatFiles: false,
     dest: '.build',
     destCss: 'css',
-    destJs: 'js',
     destFont: 'fonts',
-    concatFiles: false
-  };
+    destJs: 'js',
+    htmlPrefixCss: '',
+    htmlPrefixJs: ''
+  }
 
   var mkdir = function (path) {
-    var
-      dirs = path.split('/'),
-      prevDir = dirs.splice(0,1)+'/';
+    var dirs = path.split('/')
+    var prevDir = dirs.splice(0, 1) + '/'
 
     while (dirs.length > 0) {
-      var curDir = prevDir + dirs.splice(0,1);
+      var curDir = prevDir + dirs.splice(0, 1)
 
       if (!fs.existsSync(curDir)) {
-        fs.mkdirSync(curDir);
+        fs.mkdirSync(curDir)
       }
 
-      prevDir = curDir + '/';
+      prevDir = curDir + '/'
     }
-  };
+  }
 
   var copy = function (source, target) {
-    mkdir(path.dirname(target));
+    mkdir(path.dirname(target))
 
-    fs.writeFileSync(target, fs.readFileSync(source));
-  };
+    fs.writeFileSync(target, fs.readFileSync(source))
+  }
 
   grunt.registerMultiTask('bower-build', 'Build bower dependencies', function () {
-    var
-      cssCode = '',
-      jsCode = '',
-      htmlImportCss = '',
-      htmlImportJs = '',
-      cssMatcher = /\.css$/,
-      jsMatcher = /\.js$/,
-      fontMatcher = /\.(eot|otf|svg|ttf|woff)$/,
-      options = _.extend(defaultOptions, this.data),
-      dependencies = require('wiredep')(options.wiredep),
-      done = {};
+    var cssCode = ''
+    var jsCode = ''
+    var htmlImportCss = ''
+    var htmlImportJs = ''
+    var cssMatcher = /\.css$/
+    var jsMatcher = /\.js$/
+    var fontMatcher = /\.(eot|otf|svg|ttf|woff)$/
+    var options = _.extend(defaultOptions, this.data)
+    var dependencies = require('wiredep')(options.wiredep)
+    var done = {}
 
     // fix/extend pathes
     var initPathes = function () {
       var prependBowerPath = function (source) {
-        return path.join(process.cwd(), 'bower_components', packageName, source);
-      };
+        return path.join(process.cwd(), 'bower_components', packageName, source)
+      }
 
       var updateOther = function (other) {
-        var newOther = {};
+        var newOther = {}
 
         for (var source in other) {
-          newOther[prependBowerPath(source)] = path.join(options.dest, other[source]);
+          newOther[prependBowerPath(source)] = path.join(options.dest, other[source])
         }
 
-        return newOther;
-      };
+        return newOther
+      }
 
       for (var packageName in options.manual) {
         dependencies.packages[packageName] = {
           main: options.manual[packageName].main.map(prependBowerPath),
           other: updateOther(options.manual[packageName].other)
-        };
+        }
       }
-    };
+    }
 
     // create destination folders
     var initFolders = function () {
-      mkdir(path.join(options.dest, options.destCss));
-      mkdir(path.join(options.dest, options.destJs));
-    };
+      mkdir(path.join(options.dest, options.destCss))
+      mkdir(path.join(options.dest, options.destJs))
+    }
 
     // write CSS file to destination and add it to the HTML import list
     var addCssFile = function (name, code) {
-      var file = path.join(options.destCss, name + '.css');
+      var file = path.join(options.destCss, name + '.css')
 
       // ignore empty files
       if (code === '') {
-        return;
+        return
       }
 
-      htmlImportCss += '<link rel="stylesheet" href="' + file + '" />\n';
+      htmlImportCss += '<link rel="stylesheet" href="' + options.htmlPrefixCss + file + '" />\n'
 
-      fs.writeFileSync(path.join(options.dest, file), code);
-    };
+      fs.writeFileSync(path.join(options.dest, file), code)
+    }
 
     // write JS file to destination and add it to the HTML import list
     var addJsFile = function (name, code) {
-      var file = path.join(options.destJs, name + '.js');
+      var file = path.join(options.destJs, name + '.js')
 
       // ignore empty files
       if (code === '') {
-        return;
+        return
       }
 
-      htmlImportJs += '<script src="' + file + '"></script>\n';
+      htmlImportJs += '<script src="' + options.htmlPrefixJs + file + '"></script>\n'
 
-      fs.writeFileSync(path.join(options.dest, file), code);
-    };
+      fs.writeFileSync(path.join(options.dest, file), code)
+    }
 
     // process a single package
     var processPackage = function (name, pack) {
       // ignore already processed packages
       if (name in done) {
-        return;
+        return
       }
 
       // ignore excluded packages
       if (options.exclude && options.exclude.indexOf(name) !== -1) {
-        return;
+        return
       }
 
       // first processs dependencies
       if ('dependencies' in pack) {
         Object.keys(pack.dependencies).forEach(function (dependency) {
-          processPackage(dependency, dependencies.packages[dependency]);
-        });
+          processPackage(dependency, dependencies.packages[dependency])
+        })
       }
 
       // process file defined in main
       pack.main.forEach(function (source) {
         if (cssMatcher.test(source)) {
-          cssCode += fs.readFileSync(source).toString();
+          cssCode += fs.readFileSync(source).toString()
         } else if (jsMatcher.test(source)) {
-          jsCode += fs.readFileSync(source).toString();
+          jsCode += fs.readFileSync(source).toString()
         } else if (fontMatcher.test(source)) {
-          copy(source, path.join(options.dest, options.destFont, path.basename(source)));
+          copy(source, path.join(options.dest, options.destFont, path.basename(source)))
         } else {
-          grunt.log.writeln('unkown file type: ' + source);
+          grunt.log.writeln('unkown file type: ' + source)
         }
-      });
+      })
 
       // copy file defined in other
-      for(var source in pack.other) {
-        copy(source, pack.other[source]);
+      for (var source in pack.other) {
+        copy(source, pack.other[source])
       }
 
       // clear code if multiple files are written
       if (!options.concatFiles) {
-        addCssFile(name, cssCode);
-        addJsFile(name, jsCode);
+        addCssFile(name, cssCode)
+        addJsFile(name, jsCode)
 
-        cssCode = '';
-        jsCode = '';
+        cssCode = ''
+        jsCode = ''
       }
 
-      done[name] = true;
-    };
+      done[name] = true
+    }
 
     // process all packages
     var processPackages = function () {
       for (var packageName in dependencies.packages) {
-        processPackage(packageName, dependencies.packages[packageName]);
+        processPackage(packageName, dependencies.packages[packageName])
       }
 
       // write concatenated files
       if (options.concatFiles) {
-        addCssFile(options.name, cssCode);
-        addJsFile(options.name, jsCode);
+        addCssFile(options.name, cssCode)
+        addJsFile(options.name, jsCode)
       }
 
       // write CSS HTML import file
       if ('htmlImportCss' in options) {
-        mkdir(path.dirname(options.htmlImportCss));
-        fs.writeFile(options.htmlImportCss, htmlImportCss);
+        mkdir(path.dirname(options.htmlImportCss))
+        fs.writeFile(options.htmlImportCss, htmlImportCss)
       }
 
       // write JS HTML import file
       if ('htmlImportJs' in options) {
-        mkdir(path.dirname(options.htmlImportJs));
-        fs.writeFile(options.htmlImportJs, htmlImportJs);
+        mkdir(path.dirname(options.htmlImportJs))
+        fs.writeFile(options.htmlImportJs, htmlImportJs)
       }
-    };
+    }
 
-    initPathes();
-    initFolders();
-    processPackages();
-  });
-};
+    initPathes()
+    initFolders()
+    processPackages()
+  })
+}
